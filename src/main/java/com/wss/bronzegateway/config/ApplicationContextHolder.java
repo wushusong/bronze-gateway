4035b8be747f9756b5f89dedb0c0ab968cc67fbb
wushusong <wushusong812@163.com>
2025-08-20 16:45:56 +0800

    工具修改

--- src/main/java/com/wss/bronzegateway/config/ApplicationContextHolder.java
+++ src/main/java/com/wss/bronzegateway/config/ApplicationContextHolder.java
@@ -1,5 +1,6 @@
 package com.wss.bronzegateway.config;
 
+import lombok.Getter;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -7,15 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationContextHolder implements ApplicationContextAware {
+    @Getter
     private static ApplicationContext applicationContext;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         ApplicationContextHolder.applicationContext = applicationContext;
-    }
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
     }
 
     public static <T> T getBean(Class<T> clazz) {
